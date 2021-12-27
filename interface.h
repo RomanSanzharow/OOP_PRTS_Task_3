1358eff2f62c130207db26241d0fb4cc615bd61e
RomanSanzharow <95069728+RomanSanzharow@users.noreply.github.com>
2021-12-27 22:46:24 +0300

    Create interface.h

---
+++ interface.h
@@ -0,0 +1,33 @@
+#ifndef TASK_3_INTERFACE_H
+#define TASK_3_INTERFACE_H
+
+#include <memory>
+
+#define DIVE_MISSION 1
+#define LIFT_MISSION 2
+#define MOVE_MISSION 3
+#define RETURN_MISSION 4
+#define SHOW_MISSIONS 5
+#define SWAP_MISSIONS 6
+#define DELETE_MISSION 7
+#define INSERT_MISSION 8
+#define PUSH_DELETED_MISSION 9
+
+class Operation;
+class IMission;
+
+class interface {
+private:
+    std::unique_ptr<Operation> operation;
+
+    bool NewAction(int missionType);
+
+    static std::unique_ptr<IMission> NewMission(int missionType, bool& stop);
+
+public:
+    interface();
+
+    void startOperation();
+};
+
+#endif 
