37bc73c33a7450e321f34d9a05f3824b2fa65156
RomanSanzharow <95069728+RomanSanzharow@users.noreply.github.com>
2021-12-27 22:47:34 +0300

    Create operation.h

---
+++ operation.h
@@ -0,0 +1,44 @@
+#ifndef TASK_3_OPERATION_H
+#define TASK_3_OPERATION_H
+
+#include <string>
+#include <vector>
+
+#include "missions.h"
+
+class Operation {
+private:
+    std::vector<std::unique_ptr<IMission>> missionsList;
+    std::vector<std::unique_ptr<IMission>> deletedMissionsList;
+
+    std::unique_ptr<IMission> currentMission;
+
+public:
+    Operation() = default;
+
+    ~Operation() = default;
+
+    void Do();
+
+    void UpdateFileWithOperationData() const;
+
+    void SetMission(std::unique_ptr<IMission> mission);
+
+    void ShowOperation() const;
+
+    void ShowDeletedOperation() const;
+
+    size_t getOperationLen() const;
+
+    size_t getDeletedOperationLen() const;
+
+    void SwapMissions(size_t idx1, size_t idx2);
+
+    void DeleteMission(size_t idx);
+
+    void InsertMission(size_t idx, std::unique_ptr<IMission> mission);
+
+    void PushDeletedMission(size_t idx);
+};
+
+#endif
